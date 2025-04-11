3b9a7411f6af5e12798a404b95915167faba7fb5
amb2459103 <amboler@my.waketech.edu>
2025-04-11 15:10:50 -0400

    first commit of files

---
+++ gradebook.js
@@ -0,0 +1,12 @@
+//TODO: Fetcgh data from the PostSQL database (to be implemented later)
+function fetchGradeData() {
+  console.log("Fetching grade data ...");
+}
+
+//TODO: Populate the table with grade data
+function populateGradebook(data) {
+  console.log("Populating gradebook with data:", data);
+}
+
+const gradeData = fetchGradeData();
+populateGradebook(gradeData);
